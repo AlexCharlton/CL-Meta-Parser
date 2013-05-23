6375e387358110ec8c72ac1b8ec3a5df5edc49d8
Alex Charlton <alex.n.charlton@gmail.com>
2013-05-23 13:14:11 -0400

    Add README

---
+++ README.mk
@@ -0,0 +1,5 @@
+# CL-Meta-Parser
+
+An OMeta inspired parser written in Common Lisp.
+
+`meta-file.lisp` contains the code for the parser, documented in a literate style, as well as a grammar for an OMeta-like (but not object oriented) parser. The bootstrapped parser which which this grammer can be parsed is found in `grammer.lisp`.
